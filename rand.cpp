2fa6f4626cca9dd14577e44159886bd85b7d732a
kkpiggg <87524251+kkpiggg@users.noreply.github.com>
2023-11-15 14:41:03 +0800

    rand.cpp draft

---
+++ rand.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+#include <cstdlib>
+#include <ctime>
+
+//determine direction of the ghost (draft)
+//currently 4 direction but later should calculate the distance and then set randomization of 2 only
+int direction(){
+    time_t _time=time(nullptr);
+    srand(static_cast<unsigned>(_time));
+    for (int i=0;i<3;i++){
+        time_t _time=time(nullptr);
+        int _dir=rand()%4+1;
+        //ghost[i]=_dir;
+}        
+
+//determine how many steps the ghost will take
+int steps(){
+    time_t _time=time(nullptr);
+    srand(static_cast<unsigned>(_time));
+    for (int i=0;i<3;i++){
+        time_t _time=time(nullptr);
+        int _steps=rand()%4+1;
+        /*
+        if(check_valid(i, _steps)){
+            //code to move ghost
+        }
+        else{
+            step()
+        }
+        */
+}
+
+//check validity (make sure the ghost will not out of boundary)
+bool check_valid(int ghost_no, int steps){
+    //code
+}
