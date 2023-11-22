be698feb01ca07a90d5b0c1bf4904a19b8a72983
AnsonLai1121 <u3606767@connect.hku.hk>
2023-11-22 15:38:26 +0800

    testing

--- rand.cpp
+++ rand.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
-using namespace std;
 #include <cstdlib>
 #include <ctime>
+using namespace std;
+
 /*
+
 struct ghostObject={
     int direction;
     int vertical_ax;
-    int horizaontal_ax;
+    int horizontal_ax;
 }
 */
+
 //determine direction of the ghost (draft)
-//currently 4 direction but later we should calculate the distance and then set randomization of 2 only
+//currently 4 direction but later we should calculate the distance and then set randomisation of 2 only
 int direction(){
     time_t _time=time(nullptr);
     srand(static_cast<unsigned>(_time));
     //assume 3 ghost first, later change to the number of ghost dynamically
     for (int i=0;i<3;i++){
         time_t _time=time(nullptr);
-        int _dir=rand()%4+1;
+        // 1 is up, 2 is right, 3 is down, 4 is left
+        if (ghostObject.vertical_ax - playerObject.vertical >= 0 ){
+            if (ghostObject.horizontal_ax - playerObject.horizontal >= 0){
+                int _dir=rand()%2+3
+            } else {
+                int _dir=rand()%2+2
+            }
+        } else {
+            if (ghostObject.horizontal_ax - playerObject.horizontal >= 0) {
+                int _dir=rand()%2+1
+            } else {
+                int _dir=(rand() % 2 == 0) ? 1 : 4;
+            }
+        }
         //ghost[i].direction=_dir;
 }        
 
@@ -41,5 +57,72 @@ int steps(){
 
 //check validity (make sure the ghost will not out of boundary)
 bool check_valid(int ghost_no, int steps){
-    //code
+    //iterate over all ghosts
+    for (int i=0;i<ghost_no;i++) {
+
+        //vertical movement
+        if (ghostObject.direction == 1)  {
+            if (ghostObject.vertical_ax + steps =< plotDimension) {
+                for (int a = ghostObject.vertical_ax;a < steps;a--) {
+                    if (myPlot[a-1][ghostObject.horizontal_ax] == '_') {
+                        steps++
+                    }    
+                }
+                if (ghostObject.vertical_ax + steps =< plotDimension) {
+                    return TRUE
+                } else {
+                    return FALSE
+                }
+            } else {
+                return FALSE
+            }
+        } else if (ghostObject.direction == 3) {
+            if (ghostObject.vertical_ax - steps >= 0) {
+                for (int b = ghostObject.vertical_ax;b < steps;b++) {
+                    if (myPlot[b+1][ghostObject.horizontal_ax] == '_') {
+                        steps++
+                    }    
+                }
+                if (ghostObject.vertical_ax - steps >= 0) {
+                    return TRUE
+                } else {
+                    return FALSE
+                }
+            } else {
+                return FALSE
+            }
+
+        //horizontal movement
+        } else if (ghostObject.direction == 2) {
+            if (ghostObject.horizontal_ax + steps =< plotDimension) {
+                for (int c = ghostObject.horizontal_ax;c < steps;c++) {
+                    if (myPlot[ghostObject.vertical_ax][c+1] == '|') {
+                        steps++
+                    }
+                }
+                if (ghostObject.horizontal_ax + steps =< plotDimension) {
+                    return TRUE
+                } else {
+                    return FALSE
+                }
+            } else {
+                return FALSE
+            }
+        } else if (ghostObject.direction == 4) {
+            if (ghostObject.horizontal_ax - steps >= 0) {
+                for (int d = ghostObject.horizontal_ax;d < steps;d--) {
+                    if (myPlot[ghostObject.vertical_ax][d-1] == '|') {
+                        steps++
+                    }
+                }
+                if (ghostObject.horizontal_ax - steps >= 0) {
+                    return TRUE
+                } else {
+                    return FALSE
+                }
+            } else {
+                return FALSE
+            }
+        }
+    }       
 }
