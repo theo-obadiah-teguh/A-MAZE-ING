4429686e21116670406edd32cc2ff854d8bc444d
Fok Po Hin <146168074+BlithFok@users.noreply.github.com>
2023-11-23 13:01:49 +0800

    Update plotting.h

--- plotting.h
+++ plotting.h
@@ -4,26 +4,24 @@
 #include <iostream>
 #include <string>
 #include "players.h"
+#include "shop.h"
 using namespace std;
 
 // Clear screen function that is compatible with Windows/MacOS/Linux
 void clearscreen();
 
 // Dynamically allocate memory for the 2D plot
-string ** initPlot (string difficulty, int& plotDimension);
+string SelectPlot (string difficulty);
+
+string ** initPlot(string difficulty, int &plotDimension);
 
 // Calculate the spawn point of the player
 // Will be used for the fill plot function
-int calcPlayerSpawn(int plotDimension);
+int calcPlayerSpawn(int &row_size, int &column_size);
 
-// Temporary function to fill hypothetical maze values
-// Will be replaced by Anson's work
-void loadPlot(string **plot, int plotDimension, int spawnPoint);
-
-// Function to print out the plot which is saved in a 2D array
-void printPlot(string **plot, int plotDimension);
+void loadPlot(string ** plot, int plotDimension, int spawnPoint);
 
 // Function to animate a character's movement
-void moveAnimation(string **plot, int steps, playerObject& character, string direction, int plotDimension);
+void moveAnimation(string **plot, int steps, playerObject& character, string direction, int row_size, int column_size, int coin, int time);
 
 #endif
