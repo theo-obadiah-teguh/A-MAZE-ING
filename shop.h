2d73eb58d9da0ca6165753285c0c6fd571a654ad
Fok Po Hin <146168074+BlithFok@users.noreply.github.com>
2023-11-23 22:52:33 +0800

    Add files via upload

---
+++ shop.h
@@ -0,0 +1,10 @@
+#include "transform_array.h"
+#include <string>
+#include <iostream>
+#ifndef SHOP_H
+#define SHOP_H
+
+void purchase_options(int &coin, int &time, int &bumps, int &maxBumps);
+void visiting_shop(int &coin, int &time, int &bumps, int &maxBumps);
+
+#endif
