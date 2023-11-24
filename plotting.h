0a11d8b9583bb25bc809590e484e389f98138156
Fok Po Hin <146168074+BlithFok@users.noreply.github.com>
2023-11-24 16:58:29 +0800

    Update plotting.h to fix declaration of moveAnimation function

--- plotting.h
+++ plotting.h
@@ -24,6 +24,6 @@ int calcPlayerSpawn(int &row_size, int &column_size);
 void loadPlot(string ** plot, int plotDimension, int spawnPoint);
 
 // Function to animate a character's movement
-void moveAnimation(string **plot, int steps, playerObject& character, string direction, int row_size, int column_size, int coin, int time_limit, bool &win);
+void moveAnimation(string **plot, int steps, playerObject& character, string direction, int row_size, int column_size, int coin, int time_limit, bool &win, string exit_point);
 
 #endif
