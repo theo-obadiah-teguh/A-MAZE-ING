46ed270c3a7f8032aa0707e5f309b8242361415e
LKK-Dora <146205753+LKK-Dora@users.noreply.github.com>
2023-11-29 17:23:47 +0800

    Update monster.h

--- monster.h
+++ monster.h
@@ -12,30 +12,12 @@ struct monsterObject{
 };
 //add extern to try avoid duplicate error
 extern vector<monsterObject *> monster;
-//updated monster_count
-/*
-int monster_count;
-if (difficulty == "easy") {
-  monster_count = 3;
-}
-else if (difficulty == "medium") {
-  monster_count = 5;
-}
-else if (difficulty == "hard") {
-  monster_count = 7;
-}
- */
-/*
-vector<monsterObject *> monster;
-for (int i = 0; i < monster_count; i++){
-    monster.push_back(new monsterObject);
-}*/
-//void -> int
-void monster_no(string difficulty, int &monster_count);
-void create_monsters(int monster_count);
-void monster_dir(playerObject player);
-void init(int rowSize, int columnSize, string ** myPlot, int monster_count);
-void monster_steps(string ** myPlot, int rowSize, int columnSize);
+
+void monsterNo(string difficulty, int &monsterCount);
+void createMonsters(int monsterCount);
+void monsterDir(playerObject player);
+void init(int rowSize, int columnSize, string ** myPlot, int monsterCount);
+void monsterSteps(string ** myPlot, int rowSize, int columnSize);
 bool check_valid(int monster_count, int &monster_steps, string ** myPlot, int rowSize, int columnSize);
 //update hunt_monster function and updated in shop.cpp
 void hunt_monster(string ** array);
