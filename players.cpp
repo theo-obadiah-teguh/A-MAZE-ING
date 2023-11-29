75be542763274105bbf1f3d056b5bd77f4b2ee69
aryamanbhar <136328862+aryamanbhar@users.noreply.github.com>
2023-11-29 18:45:50 +0800

    Update players.cpp

--- players.cpp
+++ players.cpp
@@ -136,11 +136,9 @@ void movePlayer (string ** plot, int steps, playerObject& character, string dire
 	  } else {
             cout << "You don't have enough points to eat the teleporter." << endl;
 	    teleportHit = true;
-	    break;
 	  }
       }else {  
       	 teleportHit = true;
-      // break;
       }
     }
 
