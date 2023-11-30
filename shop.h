9d34193bfd8b8d03af24d1cb240fe9c39e5558ef
Fok Po Hin <146168074+BlithFok@users.noreply.github.com>
2023-11-30 09:20:00 +0800

    Update shop.h to include comments

--- shop.h
+++ shop.h
@@ -4,8 +4,10 @@
 #ifndef SHOP_H
 #define SHOP_H
 
+// Function to handle different purchase requests from players
 void purchaseOptions(int & monsterHunt, int & monsterCount, playerObject & player);
 
+// Function to direct player to the shop and ask for purchase options
 void visitShop(int & monsterHunt, int & monsterCount, playerObject & player, string exitPoint);
 
 #endif
