e81aeb44ea294734780bff308c7e296aa8e2ddea
aryamanbhar <136328862+aryamanbhar@users.noreply.github.com>
2023-11-30 20:48:22 +0800

    Update players.cpp

--- players.cpp
+++ players.cpp
@@ -162,8 +162,6 @@ void movePlayer (string ** plot, int steps, playerObject& character, string dire
       	 teleportHit = true;
 	 validAnswer = true;
 	 clearscreen();
-         printPlot(plot, rowSize, columnSize, exitPoint);
-         printPlayerStats(character);
         }
         else{
           cout << "Invalid input. Please enter 'yes' or 'no'." << endl;
