cf2c5e71e9e685dbcab203875a59f4503fdd5be0
Fok Po Hin <146168074+BlithFok@users.noreply.github.com>
2023-11-30 14:23:21 +0800

    Update main.cpp to standardize comment style

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ using namespace std::chrono;
 Function to display welcome animation or menu screen depending on whether it is the first try. 
 It will then start the maze game. 
 */
+
 void startGame(bool FirstTry){
   clearscreen();
   
@@ -27,20 +28,24 @@ void startGame(bool FirstTry){
 
 
   // Selecting a desired difficulty level
+  
   string difficulty;
   cout << "Please choose a difficulty level -> (easy, medium, hard): ";
   cin >> difficulty;
   string map = selectPlot(difficulty);
 
   // Initialize memory allocation and playerObject
+  
   int rowSize = 0, columnSize = 0, timeLimit = 180, monsterHunt;
   bool win = false;
 
   // Initialize our maze plot
+  
   string type = "maze";
   string ** myPlot = initPlot(map, type, rowSize, columnSize, difficulty);
 
   // Initialize the randomly assigned exit
+  
   string exitPoint = randomExit();
   
   if (myPlot == NULL) {
@@ -48,10 +53,12 @@ void startGame(bool FirstTry){
   }
   
   // Initialize player spawning point
+  
   int playerSpawnRow = rowSize, playerSpawnColumn = columnSize;
   calcPlayerSpawn(playerSpawnRow, playerSpawnColumn);
 
   // Initialize main player object
+  
   playerObject player = playerObject(playerSpawnRow, playerSpawnColumn, true);
   player.health = 100;
   player.coins = 150;
@@ -60,6 +67,7 @@ void startGame(bool FirstTry){
   clearscreen();
 
   // Initialize monster vector
+  
   int monsterCount = 0;
   monsterNo(difficulty, monsterCount);
     
@@ -75,11 +83,13 @@ void startGame(bool FirstTry){
   bool firstJourney = true;
 
   // Ask for player input and continue the game repeatedly until the player press 'q'
+  
   while (true) {
     string direction;
     int steps;
 
     // Describe how to control movement when the player enters the maze. Then ask player for direction to move him/her
+    
     if (firstJourney) {
       cout << "Please ENTER the 'w', 'a', 's', 'd' keys to choose a direction." << endl;
       cout << "Which direction do you want to go? (ENTER 'q' to quit) ";
@@ -160,11 +170,14 @@ void startGame(bool FirstTry){
     }
 
     // Move the player based on selected direction and move monster towards the player at each stage
+    
     monsterHunt = 0;
       
     movePlayer(myPlot, steps, player, direction, rowSize, columnSize, timeLimit, win, exitPoint, monsterCount, monsterHunt);
     prevDirection = direction;
+    
     //move monster after player move
+    
     monsterDir(player);
     monsterSteps(myPlot, rowSize, columnSize);
     clearscreen();
@@ -172,6 +185,7 @@ void startGame(bool FirstTry){
     printPlayerStats(player);
 
     // Delete dynamic array and display defeat anaimation when the player loses
+    
     if (player.health <= 0) {
       defeatAnimation();
       deleteArray(myPlot, rowSize);
@@ -179,6 +193,7 @@ void startGame(bool FirstTry){
     }
 
     // Delete dynamic array and display victory anaimation when player win the game
+      
     else if (win == true){
       victoryAnimation();
       deleteArray(myPlot, rowSize);
@@ -192,6 +207,7 @@ void startGame(bool FirstTry){
 Main function that will keep calling startGame. 
 There is an exit function in the menu function so there will not be infinite loops. 
 */
+
 int main(){
   bool FirstTry = true; 
   while (true){
